503aa1d3a35484162fc054619932bc64f36a59b5
millieba <millieba@hotmail.com>
2023-09-29 14:13:38 +0200

    #1 Extension now opens in sidebar/explorer

--- helloworld/src/extension.ts
+++ helloworld/src/extension.ts
@@ -1,26 +1,47 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+// Code based on: https://github.com/microsoft/vscode-extension-samples/tree/main/webview-view-sample 
+// and https://code.visualstudio.com/api/get-started/your-first-extension 
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "helloworld" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+  const provider = new WebViewProvider(context.extensionUri);
+  context.subscriptions.push(vscode.window.registerWebviewViewProvider(WebViewProvider.viewType, provider));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HelloWorld!');
-	});
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
+    vscode.window.showInformationMessage('Hello World from HelloWorld!');
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+class WebViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = 'helloWorld.view';
+
+  constructor(private readonly _extensionUri: vscode.Uri) { }
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    _context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    webviewView.webview.html = this._getHtmlForWebview();
+  }
+
+  private _getHtmlForWebview() {
+    return `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Hello World</title>
+      </head>
+      <body>
+          <h1>Hello World</h1>
+		      <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+      </body>
+      </html>`;
+  }
+}
