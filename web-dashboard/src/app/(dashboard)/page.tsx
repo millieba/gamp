678fb107eb8941f2b114e8cf44e493e76a49b2ed
Karen Villmones <karenvi@stud.ntnu.no>
2023-10-05 23:18:54 +0200

    #4 Simple fetch of the Github API, currently working on own repos

--- web-dashboard/src/app/(dashboard)/page.tsx
+++ web-dashboard/src/app/(dashboard)/page.tsx
@@ -1,4 +1,25 @@
-const HomePage = () => {
+import { Octokit } from 'octokit';
+
+const HomePage = async () => {
+
+  // TODO: Replace the auth with a personal access token from a personal .env file
+  const octokit = new Octokit({
+    auth: 'replaceThisWithYourOwnPersonalAccessToken'
+  });
+
+  // So far its only working on repos that you own yourself
+  try {
+    const result = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+      owner: "karenvi",
+      repo: "it3023-statistics",
+    });
+
+    console.log(result);
+  } catch (error) {
+    console.log("Did not work. Did you make sure to change 'owner' and 'repo' to a project you own and the personal auth token?")
+  }
+
+    
   return <h1 className="text-2xl">Home</h1>
 }
 
