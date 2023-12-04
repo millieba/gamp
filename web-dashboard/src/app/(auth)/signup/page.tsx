30ca78cff1d8c172cd7f7ce2ce356d9140f0a309
millieba <millieba@hotmail.com>
2023-12-04 14:18:10 +0100

    Setup next-auth. Add prisma migration.

--- web-dashboard/src/app/(auth)/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignUpPage = () => {
-    return <h1 className="text-2xl">Sign Up</h1>
-}
-
-export default SignUpPage;
---
+++ web-dashboard/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,14 @@
+import type { NextAuthOptions } from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import prisma from '@/utils/prisma'
+import GitHubProvider from 'next-auth/providers/github'
+
+export const options: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+    ],
+}
