42c2cfd1b85d115aa046ad72d7bd5b2f0531964e
millieba <millieba@hotmail.com>
2023-12-04 15:14:40 +0100

    Display session data

--- web-dashboard/src/app/api/auth/[...nextauth]/options.ts
+++ web-dashboard/src/app/api/auth/[...nextauth]/options.ts
@@ -4,11 +4,12 @@ import prisma from '@/utils/prisma'
 import GitHubProvider from 'next-auth/providers/github'
 
 export const options: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
+    // adapter: PrismaAdapter(prisma), // TODO: Make this work
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
         }),
     ],
+    secret: process.env.NEXTAUTH_SECRET as string,
 }
