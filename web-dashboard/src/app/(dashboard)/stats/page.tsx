588128062f88766e0ae75fcbea87fc57bd2cfa3d
Karen Villmones <karenvi@stud.ntnu.no>
2023-12-06 17:51:55 +0100

    Fix based on PR suggestions, created a types folder and removed usage of any in page.tsx

--- web-dashboard/src/app/(dashboard)/stats/page.tsx
+++ web-dashboard/src/app/(dashboard)/stats/page.tsx
@@ -1,44 +1,56 @@
-"use client"
+"use client";
 import StatBox from "@/components/atoms/StatBox";
 import { useEffect, useState } from "react";
+import { RepositoryDetails } from "@/utils/types";
 
 const StatsPage = () => {
-    const [fetchedData, setFetchedData] = useState<any>();
-    const [error, setError] = useState<Boolean>(false);
+  const [fetchedData, setFetchedData] = useState<RepositoryDetails[]>();
+  const [error, setError] = useState<Boolean>(false);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await fetch("/api/repositories");
-                const data = await response.json();
-                setFetchedData(data);
-            } catch (error) {
-                setError(true);
-            }
-        }
-        getData();
-    }, []);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch("/api/repositories");
+        const data = await response.json();
+        const dataDict: RepositoryDetails[] =
+          data.repos?.data.map(
+            (repo: { name: string; description: string }) => ({
+              name: repo.name,
+              description: repo.description,
+            })
+          ) || [];
+        setFetchedData(dataDict);
+      } catch (error) {
+        setError(true);
+      }
+    };
+    getData();
+  }, []);
 
-    if (error) return (<div>There was an error</div>);
+  if (error) return <div>There was an error</div>;
 
-    if (!fetchedData) return (<div>Loading...</div>);
+  if (!fetchedData) return <div>Loading...</div>;
 
-    const numberOfRepos = fetchedData.repos?.data.length;
-    
-    return (
-        <>
-        <h1 className="text-2xl">
-            Stats
-        </h1>
-        <p>Below the public repositories that you own (either created or forked), or starred are displayed. You have {numberOfRepos} public repositories.</p>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:px-4'>
-          {fetchedData.repos?.data.map((repo: any) => (
-              <StatBox title={repo.name} description={repo.description} />
-          ))}
-          </div>
-        </>
-    );
-
-}
+  const numberOfRepos = fetchedData.length;
+  console.log(fetchedData);
+  return (
+    <>
+      <h1 className="text-2xl">Stats</h1>
+      <p>
+        Below the public repositories that you own (either created or forked),
+        or starred are displayed. You have {numberOfRepos} public repositories.
+      </p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:px-4">
+        {fetchedData.map((repo: {name: string, description: string}) => (
+          <StatBox
+            key={repo.name}
+            name={repo.name}
+            description={repo.description}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default StatsPage;
--- web-dashboard/src/components/atoms/StatBox.tsx
+++ web-dashboard/src/components/atoms/StatBox.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
+import { RepositoryDetails } from '@/utils/types';
 
-interface StatBoxProps {
-  title: string;
-  description: string;
-}
-
-const StatBox: React.FC<StatBoxProps> = ({ title, description }) => {
+const StatBox: React.FC<RepositoryDetails> = ({ name, description }) => {
 
     const gradientColor = 'bg-gradient-to-tr from-gray-700 to-slate-900';
     return (
         <div className={`p-8 m-2 rounded-lg shadow-md ${gradientColor}`}>
-            <h1 className="text-2xl font-bold mb-4 pr-4 text-white">{title}</h1>
+            <h1 className="text-2xl font-bold mb-4 pr-4 text-white">{name}</h1>
             <p className="text-white pr-4">{description}</p>
         </div>
   );
