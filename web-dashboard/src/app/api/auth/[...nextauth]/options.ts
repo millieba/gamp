97d90b376ed7b8d3d57f40e43317a101f064a8ae
millieba <millieba@hotmail.com>
2024-01-16 16:26:40 +0100

    #16 Successfully delete token manually

--- web-dashboard/src/app/api/auth/[...nextauth]/options.ts
+++ web-dashboard/src/app/api/auth/[...nextauth]/options.ts
@@ -2,9 +2,34 @@ import type { NextAuthOptions, User } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from '@/utils/prisma';
-import { Account } from '@prisma/client';
 
 const adapter = PrismaAdapter(prisma);
+
+async function deleteTokenFromGitHub(accessToken: string, clientId: string, clientSecret: string) {
+    const apiUrl = `https://api.github.com/applications/${clientId}/token`;
+
+    const response = await fetch(apiUrl, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+            Accept: "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            access_token: accessToken,
+        }),
+    });
+
+    // Handle the response as needed
+    if (!response.ok) {
+        const errorMessage = `Failed to delete token from GitHub. Status: ${response.status}, ${response.statusText}`;
+        console.error(errorMessage);
+    } else {
+        console.log("Token deleted successfully");
+        // Redirect the user to the login page
+    }
+}
 
 export const options: NextAuthOptions = {
     adapter: adapter,
@@ -30,47 +55,58 @@ export const options: NextAuthOptions = {
 
             if (githubAccount.expires_at * 1000 < Date.now()) {
                 try {
-                    const refreshedToken = await refreshGitHubAccessToken(githubAccount);
+                    console.log("Deleting access token", githubAccount.access_token);
+                    // Delete the access token from GitHub
+                    await deleteTokenFromGitHub(
+                        githubAccount.access_token as string,
+                        process.env.GITHUB_ID as string,
+                        process.env.GITHUB_SECRET as string
+                    );
 
-                    await prisma.account.update({
-                        data: refreshedToken,
-                        where: {
-                            provider_providerAccountId: {
-                                provider: "github",
-                                providerAccountId: githubAccount.providerAccountId,
-                            },
-                        },
-                    });
-                } catch (error) {
-                    console.error("Error refreshing access token", error);
+                    // Force the user to re-authenticate to get a new token
                     session.error = "RefreshAccessTokenError";
+                } catch (error) {
+                    console.error("Error deleting access token", error);
+                    session.error = "DeleteAccessTokenError";
                 }
             }
 
             session.user.userId = user.id; // Add userId to the session object
             return session;
         },
-
         async signIn({ user, account }) {
             if (user && account && adapter && adapter.getUser) {
                 try {
                     const userFromDatabase = await adapter.getUser(user.id);
 
+                    // Calculate the expires_at value
+                    const expires_at = Math.floor(Date.now() / 1000) + 120; // Set expiry to 2 minutes from now
+
                     if (userFromDatabase) {
-                        await prisma.account.update({
+                        await prisma.account.upsert({
                             where: {
                                 provider_providerAccountId: {
                                     provider: account.provider,
                                     providerAccountId: account.providerAccountId,
                                 },
                             },
-                            data: {
+                            update: {
                                 access_token: account.access_token,
-                                expires_at: account.expires_at,
+                                expires_at: expires_at,
                                 id_token: account.id_token,
-                                refresh_token: account.refresh_token,
                                 session_state: account.session_state,
                                 scope: account.scope,
+                            },
+                            create: {
+                                type: 'oauth',
+                                access_token: account.access_token,
+                                expires_at: expires_at,
+                                id_token: account.id_token,
+                                session_state: account.session_state,
+                                scope: account.scope,
+                                provider: account.provider,
+                                providerAccountId: account.providerAccountId,
+                                userId: user.id,
                             },
                         });
                     }
@@ -86,40 +122,13 @@ export const options: NextAuthOptions = {
     },
 };
 
-async function refreshGitHubAccessToken(github: Account) {
-    const response = await fetch("https://github.com/login/oauth/access_token", {
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams({
-            client_id: process.env.GITHUB_ID as string,
-            client_secret: process.env.GITHUB_SECRET as string,
-            grant_type: "refresh_token",
-            refresh_token: github.refresh_token as string,
-        }),
-        method: "POST",
-    });
-
-    console.log("Refreshing a user access token with a refresh token:", response.status, response.statusText);
-    const resText = await response.text();
-    const resParams = new URLSearchParams(resText);
-    const resObject = Object.fromEntries(resParams);
-    console.log("Response Object:", resObject);
-    // If the expires_in is a string, parse it into an integer
-    const expiresIn = typeof resObject.expires_in === "string" ? parseInt(resObject.expires_in) : resObject.expires_in;
-
-    return {
-        access_token: resObject.access_token,
-        expires_at: Math.floor(Date.now() / 1000 + expiresIn),
-        refresh_token: resObject.refresh_token ?? github.refresh_token,
-    };
-}
-
 declare module "next-auth" {
-    interface ExtendedUser extends User { // Any extra fields we want to pass to the session object
+    interface ExtendedUser extends User {
         userId?: string;
     }
 
     interface Session {
-        error?: "RefreshAccessTokenError";
+        error?: "RefreshAccessTokenError" | "DeleteAccessTokenError";
         user: ExtendedUser;
     }
 }
