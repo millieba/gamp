1e1d232d7acd7de5480ed082f23ef5e8dc96a0bf
millieba <millieba@hotmail.com>
2024-01-19 13:26:48 +0100

    #16 Add sign out button to sidebar

--- web-dashboard/src/components/AuthButtons.tsx
+++ web-dashboard/src/components/AuthButtons.tsx
@@ -3,9 +3,9 @@
 import { signIn, signOut } from "next-auth/react";
 
 export const SignInButton = () => {
-    return <button onClick={() => signIn("github")}>Sign in with GitHub</button>;
+    return <button className="h-5 w-5 text-DarkNeutral1100 mr-3" onClick={() => signIn("github")}>Sign In</button>;
 };
 
 export const SignOutButton = () => {
-    return <button onClick={() => signOut()}>Sign out</button>;
+    return <button className=" text-DarkNeutral1100 mr-3" onClick={() => signOut()}>Sign Out</button>;
 };
--- web-dashboard/src/components/atoms/NavItems.tsx
+++ web-dashboard/src/components/atoms/NavItems.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { usePathname } from "next/navigation";
@@ -8,55 +7,71 @@ import {
   HomeIcon,
   StarIcon,
   UserIcon,
+  ArrowRightEndOnRectangleIcon
 } from "@heroicons/react/24/outline";
+import { SignOutButton } from "../AuthButtons";
 
-interface NavItemProps {
+interface NavItemLinkProps {
   href: string;
+  children: ReactNode;
+}
+
+interface NavItemButtonProps {
   children: ReactNode;
 }
 
 const NavItems = () => {
   const pathname = usePathname();
 
-  const NavItem = ({ href, children }: NavItemProps) => (
-    <Link
-      href={href}
-      className={`text-DarkNeutral1100 text-base font-semibold ${
-        pathname === href && "bg-DarkNeutral0 rounded-full"
-      } text-xl mb-4 px-12 py-2 relative hover:bg-DarkNeutral200 hover:rounded-full active:bg-DarkNeutral0`}
+  const NavItemLink = ({ href, children }: NavItemLinkProps) => (
+    <Link href={href}
+      className={`text-DarkNeutral1100 text-base font-semibold ${pathname === href && "bg-DarkNeutral0 rounded-full"
+        } text-xl mb-4 px-12 py-2 relative hover:bg-DarkNeutral200 hover:rounded-full active:bg-DarkNeutral0`}
     >
       <div className="flex">{children}</div>
     </Link>
   );
 
+  const NavItemButton = ({ children }: NavItemButtonProps) => (
+    <div
+      className={`text-DarkNeutral1100 font-semibold mb-4 px-12 py-2 relative rounded-full hover:bg-DarkNeutral200 hover:rounded-full`}
+    >
+      <div className="flex">{children}</div>
+    </div>
+  );
+
   return (
-  <>
+    <>
       <div className="flex flex-col">
-        <NavItem href="/">
+        <NavItemLink href="/">
           <HomeIcon className="h-5 w-5 text-DarkNeutral1100 mr-3" />
           Home
-        </NavItem>
-        <NavItem href="/badges">
+        </NavItemLink>
+        <NavItemLink href="/badges">
           <StarIcon className="h-5 w-5 text-DarkNeutral1100 mr-3" />
           Badges
-        </NavItem>
-        <NavItem href="/stats">
+        </NavItemLink>
+        <NavItemLink href="/stats">
           <ChartBarSquareIcon className="h-5 w-5 text-DarkNeutral1100 mr-3" />
           Stats
-        </NavItem>
+        </NavItemLink>
       </div>
 
       <div className="flex flex-col justify-end">
-        <NavItem href="/profile">
+        <NavItemLink href="/profile">
           <UserIcon className="h-5 w-5 text-DarkNeutral1100 mr-3" />
           Profile
-        </NavItem>
-        <NavItem href="/settings">
+        </NavItemLink>
+        <NavItemLink href="/settings">
           <Cog6ToothIcon className="h-5 w-5 text-DarkNeutral1100 mr-3" />
           Settings
-        </NavItem>
+        </NavItemLink>
+        <NavItemButton>
+          <ArrowRightEndOnRectangleIcon className="h-5 w-5 text-DarkNeutral1100 mr-3" />
+          <SignOutButton />
+        </NavItemButton>
       </div>
-  </>
+    </>
   );
 };
 
