5205ba15371e8a0df685fd56b8c99ac02d1c66e6
Karen Villmones <karenvi@stud.ntnu.no>
2024-01-25 11:42:14 +0100

    Summarized bytes for each language and displayed it as JSX in FetchLanguages

--- web-dashboard/src/components/atoms/FetchLanguages.tsx
+++ web-dashboard/src/components/atoms/FetchLanguages.tsx
@@ -3,18 +3,18 @@ import { useEffect, useState } from "react";
 import { QueryResult } from "@/app/api/languages/route";
 import { useSession, signOut } from "next-auth/react";
 
-
 const FetchLanguages = () => {
   const { data: session, status } = useSession();
   const [repositories, setRepositories] = useState<QueryResult['user']['repositories']['nodes'] | null>(null);
   const [error, setError] = useState<string | null>(null);
-  
+  const [languageSizes, setLanguageSizes] = useState<{ [key: string]: number }>({});
+
   useEffect(() => {
     if (status === 'authenticated' && session?.error === "RefreshAccessTokenError") {
       signOut();
     }
   }, [session, status]);
-  
+
   const fetchData = async () => {
     try {
       const response = await fetch("/api/languages/");
@@ -22,6 +22,19 @@ const FetchLanguages = () => {
 
       if (response.ok) {
         setRepositories(data.languages);
+
+        // Calculate language sizes
+        const sizes: { [key: string]: number } = {};
+        data.languages.forEach((repo: any) => {
+          repo.node.languages.edges.forEach((language: any) => {
+            if (sizes[language.node.name]) {
+              sizes[language.node.name] += language.size;
+            } else {
+              sizes[language.node.name] = language.size;
+            }
+          });
+        });
+        setLanguageSizes(sizes);
       } else {
         setError(data.error);
       }
@@ -37,6 +50,12 @@ const FetchLanguages = () => {
 
   return (
     <div>
+        <h1>Brief overview (to use in graphs etc.)</h1>
+        {Object.entries(languageSizes).map(([language, size], index) => (
+        <p key={index}>{language}: {size}</p>
+      ))}
+        <br></br>
+        <h1>General data fetched</h1>
       {repositories && repositories.map((repo, index) => (
         <div key={index}>
           <h2>Repository: {repo.node.name}</h2>
