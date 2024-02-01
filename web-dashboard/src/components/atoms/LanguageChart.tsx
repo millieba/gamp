0f3ebcc8b0897cc65643b397584ff6262b80f8bb
Karen Villmones <karenvi@stud.ntnu.no>
2024-02-01 18:11:28 +0100

    #34 - Implemented logic for users who do not have any repos with languages

--- web-dashboard/src/components/atoms/LanguageChart.tsx
+++ web-dashboard/src/components/atoms/LanguageChart.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
-import { QueryResult } from "@/app/api/languages/languageUtils";
 import * as d3 from "d3";
 import React from "react";
 import { calculatePercentage } from "@/utils/utils";
-import { DataItem, PieArcDatum, Language, Repository, Data } from "@/utils/types";
+import { DataItem, PieArcDatum } from "@/utils/types";
 import { ProgrammingLanguage, useSyncContext } from "@/contexts/SyncContext";
-
 
 const margin = 7;
 const height = 50;
@@ -21,16 +19,16 @@ const LanguageChart = () => {
     {}
   );
   const [hoveredSlice, setHoveredSlice] = useState<number | null>(null);
-  const { stats, isLoading } = useSyncContext();
+  const { stats } = useSyncContext();
 
   useEffect(() => {
     const sizes: { [key: string]: number } = {};
     stats?.programmingLanguages.forEach((repo: ProgrammingLanguage) => {
-        if (sizes[repo.name]) {
-          sizes[repo.name] += repo.bytesWritten;
-        } else {
-          sizes[repo.name] = repo.bytesWritten;
-        }
+      if (sizes[repo.name]) {
+        sizes[repo.name] += repo.bytesWritten;
+      } else {
+        sizes[repo.name] = repo.bytesWritten;
+      }
     });
     setLanguageSizes(sizes);
   }, [stats]);
@@ -40,20 +38,23 @@ const LanguageChart = () => {
     .map(([name, value]) => ({ name, value }))
     .sort((a, b) => b.value - a.value);
 
-  // Make a category for "other" languages, so we don't have to display all slices
-  const totalSizeOfOtherLanguages = sortedLanguages
-    .slice(4)
-    .reduce((total, item) => total + item.value, 0);
+  let languagesIncludingOther = sortedLanguages;
 
-  // New data items containing the first 4 languages and the "other" category
-  const languagesIncludingOther = [
-    ...sortedLanguages.slice(0, 4),
-    { name: "Other", value: totalSizeOfOtherLanguages },
-  ];
+  // Make a category for "other" languages, but only if there are more than 4 languages
+  if (sortedLanguages.length > 4) {
+    const totalSizeOfOtherLanguages = sortedLanguages
+      .slice(4)
+      .reduce((total, item) => total + item.value, 0);
 
-  // Creating the pie outline
+    // New data items containing the first 4 languages and the "other" category
+    languagesIncludingOther = [
+      ...sortedLanguages.slice(0, 4),
+      { name: "Other", value: totalSizeOfOtherLanguages },
+    ];
+  }
+
   const pie = d3.pie<DataItem>().value((d: DataItem) => d.value)(
-    languagesIncludingOther
+    sortedLanguages.length > 4 ? languagesIncludingOther : sortedLanguages
   );
 
   const arc = d3.arc().innerRadius(innerRadius).outerRadius(radius);
@@ -66,85 +67,98 @@ const LanguageChart = () => {
   // Sum of all bytes
   const total = d3.sum(sortedLanguages, (d: DataItem) => d.value);
 
-  return (
-        <>
-          <div
-            className="grid xs:grid-cols-1 md:grid-cols-2 justify-center"
-            style={{ maxWidth: "560px" }}
-          >
-            <div>
-              <svg
-                viewBox={`0 0 ${width / 2} ${height / 2}`}
-                className="max-w-xs mx-auto"
-              >
-                <g transform={`translate(${width / 4}, ${height / 4})`}>
-                  {pie.map((d: PieArcDatum, i: number) => (
-                    <path
-                      key={i}
-                      d={(hoveredSlice === i ? arcOver({...d, innerRadius, outerRadius}) : arc({...d, innerRadius, outerRadius})) ?? ""}
-                      fill={colors[i]}
-                      onMouseOver={() => setHoveredSlice(i)}
-                      onMouseOut={() => setHoveredSlice(null)}
-                      aria-label={`${d.data.name}, ${calculatePercentage(d.data.value, total)}%`}
-                    />
-                  ))}
-                </g>
-              </svg>
-            </div>        
+  // TODO: create logic so that this doesn't show up if there are no data
+  if (!sortedLanguages || sortedLanguages.length === 0) {
+    return <p>No data to generate graph.</p>;
+  }
 
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <svg>
-                <g>
-                  {pie.map((d: PieArcDatum, i: number) => (
-                    <g
-                      transform={`translate(0, ${i * 20})`}
-                      key={i}
-                      onMouseOver={() => setHoveredSlice(i)}
-                      onMouseOut={() => setHoveredSlice(null)}
-                    >
-                      <rect
-                        x="10"
-                        y="30"
-                        width={width / 6}
-                        height={width / 6}
-                        fill={colors[i]}
-                      />
-                      <text
-                        x="28"
-                        y="33"
-                        dy="0.35em"
-                        className={`text-xs fill-DarkNeutral1100 ${
-                          hoveredSlice === i ? `font-bold` : ``
-                        }`}
-                        onFocus={() => setHoveredSlice(i)} 
-                        onBlur={() => setHoveredSlice(null)} 
-                        tabIndex={0}
-                      >
-                        {d.data.name},{" "}
-                        {calculatePercentage(d.data.value, total)}%
-                      </text>
-                    </g>
-                  ))}
+  return (
+    <>
+      <div
+        className="grid xs:grid-cols-1 md:grid-cols-2 justify-center"
+        style={{ maxWidth: "560px" }}
+      >
+        <div>
+          <svg
+            viewBox={`0 0 ${width / 2} ${height / 2}`}
+            className="max-w-xs mx-auto"
+          >
+            <g transform={`translate(${width / 4}, ${height / 4})`}>
+              {pie.map((d: PieArcDatum, i: number) => (
+                <path
+                  key={i}
+                  d={
+                    (hoveredSlice === i
+                      ? arcOver({ ...d, innerRadius, outerRadius })
+                      : arc({ ...d, innerRadius, outerRadius })) ?? ""
+                  }
+                  fill={colors[i]}
+                  onMouseOver={() => setHoveredSlice(i)}
+                  onMouseOut={() => setHoveredSlice(null)}
+                  aria-label={`${d.data.name}, ${calculatePercentage(
+                    d.data.value,
+                    total
+                  )}%`}
+                />
+              ))}
+            </g>
+          </svg>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <svg>
+            <g>
+              {pie.map((d: PieArcDatum, i: number) => (
+                <g
+                  transform={`translate(0, ${i * 20})`}
+                  key={i}
+                  onMouseOver={() => setHoveredSlice(i)}
+                  onMouseOut={() => setHoveredSlice(null)}
+                >
+                  <rect
+                    x="10"
+                    y="30"
+                    width={width / 6}
+                    height={width / 6}
+                    fill={colors[i]}
+                  />
+                  <text
+                    x="28"
+                    y="33"
+                    dy="0.35em"
+                    className={`text-xs fill-DarkNeutral1100 ${
+                      hoveredSlice === i ? `font-bold` : ``
+                    }`}
+                    onFocus={() => setHoveredSlice(i)}
+                    onBlur={() => setHoveredSlice(null)}
+                    tabIndex={0}
+                  >
+                    {d.data.name}, {calculatePercentage(d.data.value, total)}%
+                  </text>
                 </g>
-              </svg>
-            </div>
-          </div>
-          <p>
-            Amongst your other used languages:{" "}
-            {sortedLanguages.slice(4).map((item, index, arr) => (
-              <React.Fragment key={item.name}>
-                {item.name} ({calculatePercentage(item.value, total)}%)
-                {index < arr.length - 1 ? ", " : "."}
-              </React.Fragment>
-            ))}
-          </p>
-        </>
+              ))}
+            </g>
+          </svg>
+        </div>
+      </div>
+      {sortedLanguages.length > 4 && (
+        <p>
+          Amongst your other used languages:{" "}
+          {sortedLanguages.slice(4).map((item, index, arr) => (
+            <React.Fragment key={item.name}>
+              {item.name} ({calculatePercentage(item.value, total)}%)
+              {index < arr.length - 1 ? ", " : "."}
+            </React.Fragment>
+          ))}
+        </p>
+      )}
+    </>
   );
 };
 
