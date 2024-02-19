3736d29032535f361fc92f469700c3a1a59b2081
Karen Villmones <karenvi@stud.ntnu.no>
2024-02-19 13:00:24 +0100

    Added issues, prs etc. to the infocards displayed on the stats page

--- web-dashboard/src/app/(dashboard)/stats/page.tsx
+++ web-dashboard/src/app/(dashboard)/stats/page.tsx
@@ -5,7 +5,48 @@ import { useSyncContext } from "@/contexts/SyncContext";
 import InfoCard from "@/components/atoms/InfoCard";
 
 const StatsPage = () => {
-  const { badges, isLoading } = useSyncContext();
+  const { badges, isLoading, stats } = useSyncContext();
+
+  const convertMsToDays = (ms: number) => {
+    if (ms < 86400000) {
+      const hours = ms / (1000 * 60 * 60);
+      return [hours.toFixed(0), "hours"];
+    } else {
+      const days = ms / (1000 * 60 * 60 * 24);
+      return [days.toFixed(0), "days"];
+    }
+  };
+
+  const data = [
+    {
+      subheading: "Badges earned",
+      number: badges?.length || 0,
+      unit: "badges",
+      description: `You have earned ${badges?.length > 0 ? badges?.length : 0} badges out of 3 possible!`,
+    },
+    {
+      subheading: "Issues created",
+      number: stats?.issueCount || 0,
+      unit: "issues",
+      description: `In addition, you have closed ${stats?.closedIssueCount} out of ${stats?.issueCount} issues!`,
+    },
+    {
+      subheading: "Average time on each issue",
+      number: convertMsToDays(stats?.avgTimeToCloseIssues || 0)[0],
+      unit: convertMsToDays(stats?.avgTimeToCloseIssues || 0)[1],
+      description: `On an average, you spend ${convertMsToDays(stats?.avgTimeToCloseIssues || 0)[0]} ${
+        convertMsToDays(stats?.avgTimeToCloseIssues || 0)[1]
+      } on each issue!`,
+    },
+    {
+      subheading: "Pull requests created",
+      number: stats?.createdPrs || 0,
+      unit: "pull requests",
+      description: `You have created ${stats?.createdPrs} pull requests and ${
+        stats?.createdAndMergedPrs === stats?.createdPrs ? "all" : stats?.createdAndMergedPrs
+      } of them have been merged!`,
+    },
+  ];
 
   return (
     <>
@@ -14,14 +55,20 @@ const StatsPage = () => {
         <p>Loading...</p>
       ) : (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-            <InfoCard
-              subheading="Total Badges"
-              heading="Badges"
-              number={badges?.length || 0}
-              description="You have achieved this on an average!"
-              maxWidth="300px"
-            />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+            {data.map(
+              (item, index: number) =>
+                (item.number !== 0 || item.number !== null || stats !== undefined) && (
+                  <InfoCard
+                    key={index}
+                    subheading={item.subheading}
+                    number={Number(item.number)}
+                    unit={item.unit}
+                    description={item.description}
+                    maxWidth="300px"
+                  />
+                )
+            )}
           </div>
           <StatBox
             name={"Most used languages"}
--- web-dashboard/src/components/atoms/InfoCard.tsx
+++ web-dashboard/src/components/atoms/InfoCard.tsx
@@ -2,23 +2,25 @@ import React from "react";
 
 export interface InfoDetails {
   subheading: string;
-  heading: string;
   number: number;
+  unit: string;
   description?: string;
   maxWidth?: string; // remember to specify the unit when using this prop
 }
 
-const InfoCard: React.FC<InfoDetails> = ({ subheading, heading, number, description, maxWidth }) => {
+const InfoCard: React.FC<InfoDetails> = ({ subheading, number, unit, description, maxWidth }) => {
   const gradientColor = "bg-DarkNeutral300";
   return (
-    <div className={`p-8 m-2 rounded-lg shadow-md ${gradientColor}`} style={{ maxWidth: maxWidth ? maxWidth : "" }}>
-      <h2 className="text-DarkNeutral1000">{subheading}</h2>
+    <div
+      className={`p-8 m-2 rounded-lg shadow-md ${gradientColor} overflow-visible overflow-wrap-anywhere`}
+      style={{ maxWidth: maxWidth ? maxWidth : "" }}
+    >
       <div className="flex">
         <h1 className="text-2xl font-bold pr-4 text-DarkNeutral1100">
-          {number} {heading.toLowerCase()}
+          {number} {unit}
         </h1>
       </div>
-      <p className="text-Teal600 font-bold">SOMETHING.</p>
+      <p className="text-Teal600 font-bold">{subheading}</p>
       <p className="text-DarkNeutral1100 pr-4">{description}</p>
     </div>
   );
