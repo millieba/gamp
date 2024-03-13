0f3f94c0be9b6c3d2130ce6eb7a39106a7b45a80
millieba <millieba@hotmail.com>
2024-03-13 14:53:19 +0100

    #53 Add Button and SettingBox components

---
+++ web-dashboard/src/components/atoms/settings/SettingBox.tsx
@@ -0,0 +1,16 @@
+const SettingBox = ({
+  title,
+  description,
+  children,
+}: {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}) => (
+  <div className="bg-DarkNeutral300 rounded-lg shadow-lg px-4 py-2 mb-4">
+    <h2 className="text-lg font-bold">{title}</h2>
+    <p className="text-sm my-2">{description}</p>
+    {children}
+  </div>
+);
+export default SettingBox;
