b5930e3953783daf8c1bcb775ec62b765fafaf07
Karen Villmones <karenvi@stud.ntnu.no>
2024-03-13 15:34:43 +0100

    Frontend for displaying the recent issues. Includes fallback if the user doesn't have any issues open at the moment

--- web-dashboard/src/app/(dashboard)/page.tsx
+++ web-dashboard/src/app/(dashboard)/page.tsx
@@ -1,15 +1,21 @@
 "use client";
-import RecentIssues from "@/components/atoms/issues/RecentIssues";
+import RecentIssues from "@/components/molecules/issues/RecentIssues";
+import { useSyncContext } from "@/contexts/SyncContext";
 import { useSession } from "next-auth/react";
 
 const HomePage = () => {
   const { data: session, status } = useSession();
+  const { stats, isLoading } = useSyncContext();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
       <h1 className="text-2xl">Home</h1>
       <h2 className="text-xl">{`Welcome, ${session?.user?.name}!`}</h2>
-      <RecentIssues />
+      <RecentIssues stats={stats} />
     </>
   );
 };
---
+++ web-dashboard/src/components/molecules/issues/RecentIssues.tsx
@@ -0,0 +1,58 @@
+import { AssignedIssueInterface } from "@/app/api/issues/issuesUtils";
+import { Stats } from "@/contexts/SyncContext";
+import React from "react";
+
+export interface RecentIssuesProps {
+  stats: Stats | undefined;
+}
+
+const RecentIssues: React.FC<RecentIssuesProps> = ({ stats }) => {
+  let openIssues: AssignedIssueInterface[] = [];
+  let closedIssues: AssignedIssueInterface[] = [];
+
+  // Check if stats and assignedIssues exist
+  if (stats && stats.assignedIssues) {
+    // Sort by dat in descending order, this will help display the most recent open issues first
+    const sortedIssues = [...stats.assignedIssues].sort(
+      (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    );
+
+    // Want to separate between open and closed issues, therefore we filter the sortedIssues array
+    openIssues = sortedIssues.filter((issue) => issue.state === "OPEN");
+    closedIssues = sortedIssues.filter((issue) => issue.state === "CLOSED");
+  }
+
+  const getRepoNameFromUrl = (url: string) => {
+    const urlSplit = url.split("/");
+    return urlSplit[urlSplit.length - 3];
+  };
+
+  return (
+    <div className={`p-4 rounded-lg shadow-md bg-DarkNeutral100 w-full grid grid-row`}>
+      {openIssues.length === 0 ? (
+        <span className="mb-4">No recent issues assigned to you right now &#x1F60A;</span>
+      ) : (
+        <>
+          <span className="mb-4">Recent issues you've been assigned to &#129299;</span>
+          <div className="max-h-[200px] overflow-auto">
+            {openIssues.map((issue, index) => (
+              <a href={issue.url} target="_blank" rel="noopener noreferrer" key={index}>
+                <div
+                  className={`p-2 mb-2 rounded-lg shadow-md bg-DarkNeutral300 hover:bg-DarkNeutral350 flex flex-col sm:flex-grow 2xl:flex-grow-0`}
+                >
+                  <h1 className="font-bold">
+                    Issue #{issue.number} in {getRepoNameFromUrl(issue.url)}
+                  </h1>
+                  <p>{issue.title}</p>
+                </div>
+              </a>
+            ))}
+          </div>
+        </>
+      )}
+      {/* IDEA: Maybe we could add a issue-related badge the user is close to achieving at the bottom to make it more tempting to achieve it? */}
+    </div>
+  );
+};
+
+export default RecentIssues;
