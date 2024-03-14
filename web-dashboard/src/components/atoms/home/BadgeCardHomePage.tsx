75eda765d385817742e68cad6d4aa040fb9dbb39
Karen Villmones <karenvi@stud.ntnu.no>
2024-03-14 14:57:51 +0100

    Added progress bar to badges that the user is close to approach

--- web-dashboard/src/components/atoms/home/BadgeCardHomePage.tsx
+++ web-dashboard/src/components/atoms/home/BadgeCardHomePage.tsx
@@ -25,31 +25,28 @@ const BadgeCardHomePage: React.FC<BadgeDetails> = ({
   unit = "",
 }) => {
   const bgColor = "bg-DarkNeutral300";
+  const percentage = Math.floor((progress / threshold) * 100);
 
   const messages = [
     `You're only ${threshold - progress} ${threshold - progress === 1 ? unit.slice(0, -1) : unit} away!`,
     `Keep going, only ${threshold - progress} ${threshold - progress === 1 ? unit.slice(0, -1) : unit} to go!`,
-    `Make ${threshold - progress} ${threshold - progress === 1 ? unit.slice(0, -1) : unit} to achieve this`,
+    `Make ${threshold - progress} ${threshold - progress === 1 ? unit.slice(0, -1) : unit} more to achieve this`,
   ];
 
   const randomIndex = Math.floor(Math.random() * messages.length);
   const randomMessage = messages[randomIndex];
 
   return (
-    <div className={`p-2 rounded-lg shadow-md flex flex-col sm:flex-row ${bgColor} max-w-[350px] m-2`}>
+    <div className={`p-2 rounded-lg shadow-md flex flex-col md:flex-row ${bgColor} max-w-[350px] m-2`}>
       <div className="flex flex-col justify-center items-center">
-        <img src={image} alt="Badge" className="mb-2 w-[50px] bouncy" />
+        <img src={image} alt="Badge" className="mb-2 min-w-[50px] w-[50px] bouncy" />
       </div>
       <div className="ml-4 items-center text-center sm:text-left">
         <div className="flex items-center">
           <h1 className="text-sm font-bold mb-1 text-DarkNeutral1000">
             {name} ({points} XP)
           </h1>
-          {achieved ? (
-            <CheckCircleIcon className="w-[18px] h-[18px] text-Lime500 ml-1 mb-1" />
-          ) : (
-            <ClockIcon className="w-[18px] h-[18px] text-Red500 ml-1 mb-1" />
-          )}
+          {achieved && <CheckCircleIcon className="w-[18px] h-[18px] text-Lime500 ml-1 mb-1" />}
         </div>
         <p className="text-DarkNeutral1100 mb-1 text-xs">{description}</p>
 
@@ -58,7 +55,19 @@ const BadgeCardHomePage: React.FC<BadgeDetails> = ({
             Achieved: {date ? new Date(date).toISOString().split("T")[0] : ""}
           </div>
         ) : (
-          <div className="text-DarkNeutral1000 text-xs xs:text-center">{randomMessage}</div>
+          <div className="items-center text-center md:items-left md:text-left w-full">
+            <div className="flex justify-between text-DarkNeutral1000 md:w-[200px] w-full">
+              <span className="text-xs">0</span>
+              <span className="text-xs">{threshold}</span>
+            </div>
+            <div className="w-full md:w-[200px] bg-DarkNeutral500 rounded-full h-1 text-DarkNeutral1000 items-center text-center md:items-left md:text-left">
+              <div
+                className="bg-Lime500 h-1 rounded-full text-DarkNeutral1000"
+                style={{ width: `${percentage}%` }}
+              ></div>
+            </div>
+            <div className="text-DarkNeutral1000 text-xs mt-2 text-center md:text-left ">{randomMessage}</div>
+          </div>
         )}
       </div>
     </div>
