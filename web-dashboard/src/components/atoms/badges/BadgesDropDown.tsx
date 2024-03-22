de1f9886a5b396cf1be58736626776c166b825e2
Karen Villmones <karenvi@stud.ntnu.no>
2024-03-22 12:29:10 +0100

    Just a cursor pointer on the selected tags

--- web-dashboard/src/components/atoms/badges/BadgesDropDown.tsx
+++ web-dashboard/src/components/atoms/badges/BadgesDropDown.tsx
@@ -92,7 +92,7 @@ const BadgesDropDown = () => {
                 <div
                   key={tag}
                   className="bg-DarkNeutral1100 hover:bg-DarkNeutral1000 rounded-full w-fit py-1.5 px-3 border border-DarkNeutral350 text-DarkNeutral350
-                    flex items-center gap-2"
+                    flex items-center gap-2 cursor-pointer"
                   onMouseDown={(e) => e.preventDefault()}
                   onClick={() => setSelectedTags(selectedTags.filter((i) => i !== tag))}
                 >
