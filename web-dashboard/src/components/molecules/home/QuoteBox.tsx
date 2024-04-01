d157eba7fb3531f0492bb77172a6c025e85f5666
millieba <millieba@hotmail.com>
2024-04-01 14:23:43 +0200

    #101 Add skip button with tooltip

--- web-dashboard/src/components/molecules/home/QuoteBox.tsx
+++ web-dashboard/src/components/molecules/home/QuoteBox.tsx
@@ -1,6 +1,7 @@
 "use client";
 import StatCard from "@/components/atoms/StatCard";
 import { useEffect, useState } from "react";
+import Button from "@/components/atoms/Button";
 import { Quote } from "../../../../prisma/seed";
 
 const QuoteBoxSkeleton = () => (
@@ -20,6 +21,9 @@ const QuoteBoxSkeleton = () => (
 const QuoteBox = () => {
   const [quote, setQuote] = useState<Quote>();
   const [isLoading, setIsLoading] = useState(true);
+  const [isSkipButtonDisabled, setIsSkipButtonDisabled] = useState(false);
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [error, setError] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,24 +35,49 @@ const QuoteBox = () => {
     fetchData();
   }, []);
 
+  const handleSkipQuote = async () => {
+    setIsLoading(true);
+    const res = await (await fetch("api/quote/skip")).json();
+    if (res?.error) {
+      setIsSkipButtonDisabled(true);
+      setIsLoading(false);
+      setError(res.error);
+      return;
+    }
+    setQuote(res);
+    setIsLoading(false);
+  };
+
   return isLoading ? (
     <QuoteBoxSkeleton />
   ) : (
-    <StatCard
-      name={
-        quote?.type === "quote"
-          ? "Quote of the Day"
-          : quote?.type === "affirmation"
-          ? "Affirmation of the Day"
-          : "Tip of the Day"
-      }
-      content={
-        <div className="text-DarkNeutral1100">
-          <p className="text-lg mb-3">{quote?.text}</p>
-          {quote?.source && <p className="text-sm">- {quote.source}</p>}
+    <div
+      onMouseEnter={() => setTooltipVisible(true)}
+      onMouseLeave={() => setTooltipVisible(false)}
+      className="relative w-full "
+    >
+      <StatCard
+        name={
+          quote?.type === "quote"
+            ? "Quote of the Day"
+            : quote?.type === "affirmation"
+            ? "Affirmation of the Day"
+            : "Tip of the Day"
+        }
+        content={
+          <div className="text-DarkNeutral1100">
+            <p className="text-lg mb-3">{quote?.text}</p>
+            {quote?.source && <p className="text-sm">- {quote.source}</p>}
+            <Button label="Skip Quote" clickHandler={handleSkipQuote} isDisabled={isSkipButtonDisabled} />
+          </div>
+        }
+      />
+      {tooltipVisible && error && (
+        <div className="absolute bg-DarkNeutral400 text-DarkNeutral1000 p-2 rounded-md shadow-lg z-10 max-w-[60%] bottom-5 left-40 mr-3">
+          {error}
         </div>
-      }
-    />
+      )}
+    </div>
   );
 };
 
